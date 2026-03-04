efb2caec9d99640f46c454da6429ec7caa53290d
Mark Dujali <mddujali@gmail.com>
2026-03-04 17:17:18 +0800

    Add Spatie Ray

---
+++ ray.config.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+
+const env = dotenv.config();
+expand(env);
+
+import { execSync } from 'node:child_process';
+
+const isWsl = execSync('grep -i microsoft /proc/version', { stdio: 'pipe' }).toString().length > 0;
+
+const host = isWsl
+  ? execSync("ip route show default | awk '{print $3}'", { stdio: 'pipe' })
+    .toString()
+    .trim()
+  : 'localhost';
+
+export default {
+  enable: process.env.RAY_ENABLE === 'true',
+  host,
+  port: process.env.RAY_PORT || 23517,
+  scheme: process.env.RAY_SCHEME || 'http',
+};
--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,20 @@ const env = dotenv.config();
 expand(env);
 
 import { Bot } from 'grammy';
+import { Ray, ray } from 'node-ray';
+
+await Ray.initSettings();
 
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
 bot.command('start', async (ctx) => {
-  console.log(ctx);
+  ray(ctx);
 
   await ctx.reply('Welcome! Up and running.');
 });
 
 bot.on('message', async (ctx) => {
-  console.log(ctx);
+  ray(ctx);
 
   await ctx.reply('Got another message!');
 });
@@ -25,6 +28,8 @@ async function main() {
 }
 
 main().catch((err) => {
+  ray(err).red();
+
   console.error('Error starting the bot:', err);
 
   process.exit(1);
